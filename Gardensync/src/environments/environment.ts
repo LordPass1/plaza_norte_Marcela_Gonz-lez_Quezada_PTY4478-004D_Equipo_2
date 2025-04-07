4d9d9bbb5f458399e5872c82cbe860a7d0a1a721
DiegoGodoy0 <dieg.godoy@duocuc.cl>
2025-04-07 15:46:19 -0400

    Firebase y Firestore no configurados
    Se realizan las instalaciones y primeras configuraciones de Firebase y Firestore para utilizarse como servicio de base de datos

--- Gardensync/src/environments/environment.ts
+++ Gardensync/src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  treffleApiKey: 'VC81-hHdzJOmyKFhDd1NQt58D0vZulAe-KC5YmwgZ-g'
+  treffleApiKey: 'VC81-hHdzJOmyKFhDd1NQt58D0vZulAe-KC5YmwgZ-g',
+  firebase: {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID",
+    measurementId: "TU_MEASUREMENT_ID" // Opcional para Analytics
+  }
 };
 
 /*
