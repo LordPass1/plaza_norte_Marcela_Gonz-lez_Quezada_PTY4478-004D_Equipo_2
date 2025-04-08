c8e9bba587f4f65e5cab8f0168473e27249e436d
DiegoGodoy0 <dieg.godoy@duocuc.cl>
2025-04-08 16:51:45 -0400

    Mucho intento

--- Gardensync/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ Gardensync/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,13 @@
-package io.ionic.starter;
-
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthentication;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Registra manualmente el plugin
+        registerPlugin(FirebaseAuthentication.class);
+    }
+}
