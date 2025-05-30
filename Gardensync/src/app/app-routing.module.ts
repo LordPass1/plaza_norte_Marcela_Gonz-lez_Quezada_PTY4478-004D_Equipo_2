9aca27097385175747460609e17a4e637cd61d2d
TuGatita777 <maxi.gonzalezg@duocuc.cl>
2025-05-30 02:40:29 -0400

    revertir

--- Gardensync/src/app/app-routing.module.ts
+++ Gardensync/src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -48,6 +49,56 @@ const routes: Routes = [
     loadChildren: () => import('./pages/perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
   },
   {
+=======
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'iniciar-persona',
+    loadChildren: () => import('./pages/iniciar-persona/iniciar-persona.module').then( m => m.IniciarPersonaPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'registro',
+    pathMatch: 'full'
+  },
+  {
+    path: 'g-maceta',
+    loadChildren: () => import('./pages/g-maceta/g-maceta.module').then( m => m.GMacetaPageModule)
+  },
+  {
+    path: 'add-new',
+    loadChildren: () => import('./pages/add-new/add-new.module').then( m => m.AddNewPageModule)
+  },
+  {
+    path: 'p-principal',
+    loadChildren: () => import('./pages/p-principal/p-principal.module').then( m => m.PPrincipalPageModule)
+  },
+  {
+    path: 'p-consejos',
+    loadChildren: () => import('./pages/p-consejos/p-consejos.module').then( m => m.PConsejosPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },{
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'registro-hogar',
+    loadChildren: () => import('./pages/registro-hogar/registro-hogar.module').then( m => m.RegistroHogarPageModule)
+  },
+  {
+    path: 'perfil-usuario',
+    loadChildren: () => import('./pages/perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
+  },  {
+>>>>>>> parent of 7277d51 (modificaciones)
     path: 'agregarmaceta',
     loadChildren: () => import('./pages/agregarmaceta/agregarmaceta.module').then( m => m.AgregarmacetaPageModule)
 =======
@@ -105,6 +156,7 @@ const routes: Routes = [
   },
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 ];
@@ -117,6 +169,9 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 =======
+=======
+
+>>>>>>> parent of 7277d51 (modificaciones)
 
 ];
 
@@ -127,4 +182,7 @@ export class AppRoutingModule { }
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+<<<<<<< HEAD
 >>>>>>> parent of de3eab8 (algunas paginas subidas)
+=======
+>>>>>>> parent of 7277d51 (modificaciones)
